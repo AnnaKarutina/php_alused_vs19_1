8e0ba96780e84f44048330da0ad19f011913fe0e
Anna Karutina <anna.karutina@khk.ee>
2019-12-09 16:54:32 +0200

    koodi taaskasutus - Ülesanne 10 - näidislahendused

---
+++ page/fcs.php
@@ -0,0 +1,33 @@
+<?php
+
+use function Sodium\increment;
+
+/**
+ * file name: fcs.php;
+ * autor: anna.karutina;
+ * date: 09.12.2019;
+ */
+function pageMenu($menuData){
+  foreach ($menuData as $menuName => $fileName){
+    $link = 'index.php';
+    if($fileName != 'index'){
+      $link .= '?leht='.$fileName;
+    }
+    echo '<a href="'.$link.'">'.$menuName.'</a>&nbsp;&nbsp;';
+  }
+}
+
+function control($pageName, $menuData){
+  if(strlen($pageName) > 0 and array_search($pageName, $menuData)){
+    return true;
+  }
+  return false;
+}
+
+function pageContent($pageName, $menuData) {
+  if(control($pageName, $menuData)) {
+    include($pageName.'.php');
+  } else {
+    echo '<h2>Pöördusid vale lehe poole!</h2>';
+  }
+}
