a703a691f975f2681fab538df4a7575b1f903b42
Anna Karutina <anna.karutina@khk.ee>
2019-12-19 12:10:18 +0200

    sessioon - pealeht kasutajate registreerimiseks ja sisselogimiseks ja abifailid

---
+++ session/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * file name: index.php;
+ * autor: anna.karutina;
+ * date: 19.12.2019;
+ */
+echo '<a href="add.php">Registreeri kasutajaks</a>';
+echo '&nbsp;&nbsp;&nbsp;';
+echo '<a href="login.php">Logi sisse</a>';
