72477915f5d71470fb9e594c2bdda07ffbdeb4fd
Anna Karutina <anna.karutina@khk.ee>
2019-12-19 12:36:06 +0200

    sessioon - kasutaja sisse logimine ja sessiooni avamine

--- session/index.php
+++ session/index.php
@@ -4,6 +4,11 @@
  * autor: anna.karutina;
  * date: 19.12.2019;
  */
-echo '<a href="add.php">Registreeri kasutajaks</a>';
-echo '&nbsp;&nbsp;&nbsp;';
-echo '<a href="login.php">Logi sisse</a>';
+session_start();
+if(empty($_SESSION['kasutaja'])){
+  echo '<a href="add.php">Registreeri kasutajaks</a>';
+  echo '&nbsp;&nbsp;&nbsp;';
+  echo '<a href="login.php">Logi sisse</a>';
+} else {
+  echo 'Tere tulemast, '.$_SESSION['kasutaja'].'<br>';
+}
